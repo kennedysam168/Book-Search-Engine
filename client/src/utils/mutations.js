d105f4a1eeb1991b83ae5351ac6a4cdbfc2fd304
Sam Kennedy <kennedysam168@gmail.com>
2022-02-10 20:35:26 -0500

    added changes

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,89 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+          }
+        }
+      }
+    }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($email: String!, username: String!, $password: String!) {
+    addUser(email: $email, username: $username, password: $password) {
+        token
+        user {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+          }
+        }
+      }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: bookInput!) {
+    saveBook(input: $input) {
+        token
+        user {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+          }
+        }
+      }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        token
+        user {
+          _id
+          username
+          email
+          bookCount
+          savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+          }
+        }
+      }
+    }
+`;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  type Query {
+  me: User
+  }
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User
+    removeBook(bookId: String!): User
+  }
+  input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+}
+type Auth {
+  token: ID
+  user: User
+}
+`;
+
+module.exports = typeDefs;
